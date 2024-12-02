ebfa94f341c0d3ce95a0b112046c6963ec087ffe
ElizabethCcahua <eccahuach@unsa.edu.pe>
2024-12-02 05:58:35 -0500

    configurar url en urls.py de la aplicacion

---
+++ mi_proyecto/tienda/urls.py
@@ -0,0 +1,9 @@
+# tienda/urls.py
+from django.urls import path
+from . import views  # Importa las vistas de la aplicación
+
+# Define las rutas para las vistas 'crear_venta' y 'lista_ventas'
+urlpatterns = [
+    path('crear_venta/', views.crear_venta, name='crear_venta'),  # Ruta para crear una venta
+    path('lista_ventas/', views.lista_ventas, name='lista_ventas'),  # Ruta para listar todas las ventas
+]
